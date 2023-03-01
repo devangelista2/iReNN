d7c78b13a31f399f6230bb79184b9245fd7b4005
devangelista2 <davide.evangelista5@unibo.it>
2023-03-01 11:54:52 +0100

    Added support to fanflat

--- test_iCP_TV.py
+++ test_iCP_TV.py
@@ -25,7 +25,7 @@ x_true = transform.resize(x_true, (256, 256))
 M, N = x_true.shape
 
 # Forward problem
-A = operators.MatrixOperator(io.loadmat("./data/A.mat")['A'])
+A = operators.CTProjector(256, 256, np.linspace(0, np.pi, 120), det_size=367, geometry='parallel')
 
 # Noiseless y
 y = A(x_true)
@@ -36,7 +36,7 @@ e = get_gaussian_noise(y, noise_level=settings.forward_problem['noise_level'])
 y_delta = y + e
 
 # Choose the parameters
-algorithm = 'CP_TV'
+algorithm = 'iCP_TV'
 match algorithm:
     case 'iCP_TV':
         parameters = settings.iCP_TV_algorithm
@@ -58,4 +58,4 @@ solver = iCP_TV(A, (M, N))
 x_sol = solver(y_delta, epsilon, lmbda, H=H, K=K, x_true=x_true, p=p)
 
 # Save the solution
-np.save(f'./results/COULE_{algorithm}_{idx}.npy', x_sol)
+# np.save(f'./results/COULE_{algorithm}_{idx}.npy', x_sol)
